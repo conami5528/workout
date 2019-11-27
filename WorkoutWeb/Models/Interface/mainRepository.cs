18556d85b838d6d866bbf9532c4e80489343560e
WenChung.Lu <as369000@yahoo.com.tw>
2019-11-27 16:33:31 +0800

    加入專案檔案。

---
+++ WorkoutWeb/Models/Interface/mainRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+
+
+namespace WorkoutWeb.Models.Interface
+{
+    public interface MainRepository<Tentity>
+    {
+        void Create(Tentity QQ);
+        void Update(Tentity QQ);
+        void Delete(Tentity QQ) ;
+        Tentity GetT(int UserId);
+        IQueryable<Tentity> GetAll();
+        void SaveChanges();
+
+        
+    }
+}
---
+++ WorkoutWeb/Models/Repository/GenericRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WorkoutWeb.Models.Interface;
+
+namespace WorkoutWeb.Models.Repository
+{
+    public class GenericRepository<Tentity> :  MainRepository<Tentity>
+    {
+        public void Create(Tentity instance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Tentity instance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Tentity instance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Tentity GetT(int primaryID) 
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Tentity> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
